32676b3795acc982537e56318b620c7e4c4d4128
Mart <martoudeweernink@gmail.com>
2017-04-28 11:18:42 +0200

    Add drone to bundle.sh

--- bundles/bundle.sh
+++ bundles/bundle.sh
@@ -11,3 +11,4 @@ cp "$ROOT/pubsub/javaserializer/target/java-serializer-0.1.jar" $TARGET
 cp "$ROOT/pubsub/rabbitmq/subscriber/target/rabbitmq-subscriber-0.1.jar" $TARGET
 cp "$ROOT/pubsub/rabbitmq/publisher/target/rabbitmq-publisher-0.1.jar" $TARGET
 cp "$ROOT/physicsengine/target/physicsengine-0.1.jar" $TARGET
+cp "$ROOT/drone/target/drone-0.1.jar" $TARGET

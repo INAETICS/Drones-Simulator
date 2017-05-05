f6bdeab195ee79d3f032899c39889852844be3b2
Wybren Kortstra <wybren.kortstra@gmail.com>
2017-05-05 15:23:26 +0200

    Base image and engine image

--- docker_images/create_docker_images.sh
+++ docker_images/create_docker_images.sh
@@ -3,5 +3,7 @@
 ROOT=".."
 TARGET="./game_engine/files"
 
+docker build ./base_image/ -t dronesim/base
+
 cp "$ROOT/bundles/"*".jar" "$TARGET/bundles/"
 cp "$ROOT/dependent-bundles/"*".jar" "$TARGET/dependencies/"

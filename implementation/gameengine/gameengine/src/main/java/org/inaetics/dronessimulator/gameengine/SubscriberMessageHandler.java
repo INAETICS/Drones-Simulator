c7793269ff7550f4f3cc0c9a0364cbbbb1c76344
Sebastiaan la Fleur <sebastiaan@slafleur.nl>
2017-05-19 20:48:29 +0200

    Added missing messages to subscribermessagehandler and accompanying tests

--- implementation/gameengine/gameengine/src/main/java/org/inaetics/dronessimulator/gameengine/SubscriberMessageHandler.java
+++ implementation/gameengine/gameengine/src/main/java/org/inaetics/dronessimulator/gameengine/SubscriberMessageHandler.java
@@ -43,7 +43,7 @@ public class SubscriberMessageHandler implements MessageHandler {
             Optional<D3Vector> maybeAcceleration = movementMessage.getAcceleration();
 
             if(maybeAcceleration.isPresent()) {
-                physicsEngineDriver.changeAccelerationEntity(id_mapper.fromProtocolToGameEngineId(movementMessage.getIdentifier()), maybeAcceleration.get());
+                physicsEngineDriver.changeAccelerationEntity(movementMessage.getIdentifier(),  maybeAcceleration.get());
             } else {
                 Logger.getLogger(SubscriberMessageHandler.class).error("Received movement message without acceleration for drone " + movementMessage.getIdentifier() + ". Received: " + message);
             }
@@ -54,12 +54,12 @@ public class SubscriberMessageHandler implements MessageHandler {
         } else if(message instanceof DamageMessage) {
             DamageMessage damageMessage = (DamageMessage) message;
 
-            physicsEngineDriver.damageEntity(id_mapper.fromProtocolToGameEngineId(damageMessage.getEntityId()), damageMessage.getDamage());
+            physicsEngineDriver.damageEntity(damageMessage.getEntityId(), damageMessage.getDamage());
         } else if(message instanceof KillMessage) {
             // Kill the entity
             KillMessage killMessage = (KillMessage) message;
 
-            physicsEngineDriver.removeEntity(id_mapper.fromProtocolToGameEngineId(killMessage.getIdentifier()));
+            physicsEngineDriver.removeEntity(killMessage.getIdentifier());
         } else if(message instanceof FireBulletMessage) {
             FireBulletMessage fireBulletMessage = (FireBulletMessage) message;
 
@@ -71,14 +71,13 @@ public class SubscriberMessageHandler implements MessageHandler {
             Optional<D3Vector> maybeAcceleration = fireBulletMessage.getAcceleration();
 
 
-            if(maybePosition.isPresent() && maybeVelocity.isPresent() && maybeAcceleration.isPresent()) {
+            if(fireBulletMessage.getType().equals(EntityType.BULLET) && maybePosition.isPresent() && maybeVelocity.isPresent() && maybeAcceleration.isPresent()) {
                 GameEntity firedBy = stateManager.getById(id_mapper.fromProtocolToGameEngineId(fireBulletMessage.getFiredById()));
                 Bullet bullet = new Bullet(gameEngineId, fireBulletMessage.getDamage(), firedBy, maybePosition.get(), maybeVelocity.get(), maybeAcceleration.get());
 
-                id_mapper.setMapping(gameEngineId, fireBulletMessage.getIdentifier());
-                physicsEngineDriver.addNewEntity(bullet);
+                physicsEngineDriver.addNewEntity(bullet, fireBulletMessage.getIdentifier());
             } else {
-                Logger.getLogger(SubscriberMessageHandler.class).error("Received a fire bullet but the position, velocity or acceleraiton is not set: " + fireBulletMessage);
+                Logger.getLogger(SubscriberMessageHandler.class).error("Received a fire bullet but the type, position, velocity or acceleration is not set: " + fireBulletMessage);
             }
         } else {
             Logger.getLogger(SubscriberMessageHandler.class).error("Received a message which is not a movement message. Do not know what to do with it. Received: " + message.getClass().getName() + " " + message);

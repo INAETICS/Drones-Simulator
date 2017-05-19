bdc393d8582dfaca6f062188c79da3c0342c25ec
Sebastiaan la Fleur <sebastiaan@slafleur.nl>
2017-05-19 12:53:26 +0200

    Refactored CreateEntity to *Message and let it extend of ProtocolMessage

--- implementation/common/src/main/java/org/inaetics/dronessimulator/common/protocol/CreateEntityMessage.java
+++ implementation/common/src/main/java/org/inaetics/dronessimulator/common/protocol/CreateEntityMessage.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class CreateEntity {
+public class CreateEntityMessage extends ProtocolMessage {
     /** Indentifier of object */
     private String identifier = null;
 

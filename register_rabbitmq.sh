ffb7e3a341cb5d6335d1344f33c7fd36376014e7
Sebastiaan la Fleur <sebastiaan@slafleur.nl>
2017-05-28 16:44:25 +0200

    Added guest/guest user to local rabbitmq for local environment

--- register_rabbitmq.sh
+++ register_rabbitmq.sh
@@ -12,8 +12,8 @@ RABBITMQ_PATH="amqp://localhost:5672"
 
 # Actually set values
 etcdctl set $INSTANCE_DIR/uri $RABBITMQ_PATH > /dev/null
-etcdctl set $CONFIG_DIR/$CONFIG_ID $INSTANCE_DIR > /dev/null
-
+etcdctl set $INSTANCE_DIR/username "guest" > /dev/null
+etcdctl set $INSTANCE_DIR/password "guest" > /dev/null
 # Confirmation
 echo "RabbitMQ registered with URI $RABBITMQ_PATH"
 

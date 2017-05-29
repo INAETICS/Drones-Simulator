3950f01588d22042eac1965f514b1eaaf68e575d
Sebastiaan la Fleur <sebastiaan@slafleur.nl>
2017-05-29 16:25:27 +0200

    Text above height is now removed when a drone is killed in visualisation

--- implementation/visualisation/src/main/java/org/inaetics/dronessimulator/visualisation/uiupdates/RemoveDrone.java
+++ implementation/visualisation/src/main/java/org/inaetics/dronessimulator/visualisation/uiupdates/RemoveDrone.java
@@ -2,16 +2,20 @@ package org.inaetics.dronessimulator.visualisation.uiupdates;
 
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 
 public class RemoveDrone extends UIUpdate {
     public final ImageView imageView;
+    public final Text heightText;
 
-    public RemoveDrone(ImageView imageView) {
+    public RemoveDrone(ImageView imageView, Text heightText) {
         this.imageView = imageView;
+        this.heightText = heightText;
     }
 
     @Override
     public void execute(Pane pane) {
         pane.getChildren().remove(imageView);
+        pane.getChildren().remove(heightText);
     }
 }

4b535d32bbe76d5d1597c330d8334f5dd48a2138
Sebastiaan la Fleur <sebastiaan@slafleur.nl>
2017-05-30 12:10:09 +0200

    Increased visualisation window size again

--- implementation/visualisation/src/main/java/org/inaetics/dronessimulator/visualisation/Settings.java
+++ implementation/visualisation/src/main/java/org/inaetics/dronessimulator/visualisation/Settings.java
@@ -5,8 +5,8 @@ public class Settings {
     public static int SCENE_WIDTH = 1024;
     public static int SCENE_HEIGHT = 768;
 
-    public static int CANVAS_WIDTH = 300;
-    public static int CANVAS_HEIGHT = 300;
+    public static int CANVAS_WIDTH = 1500;
+    public static int CANVAS_HEIGHT = 1500;
 
     public static int DRONE_WIDTH = 128;
     public static int DRONE_HEIGHT = 128;

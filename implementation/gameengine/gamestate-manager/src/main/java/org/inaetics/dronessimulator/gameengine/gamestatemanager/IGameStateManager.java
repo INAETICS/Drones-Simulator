3c828559ab4bc0f7ccea2be927e4d51c3ea9c80c
Jan-Jelle Kester <janjelle@jjkester.nl>
2017-06-23 14:26:51 +0200

    Document gameengine-gamestate-manager

--- implementation/gameengine/gamestate-manager/src/main/java/org/inaetics/dronessimulator/gameengine/gamestatemanager/IGameStateManager.java
+++ implementation/gameengine/gamestate-manager/src/main/java/org/inaetics/dronessimulator/gameengine/gamestatemanager/IGameStateManager.java
@@ -2,10 +2,26 @@ package org.inaetics.dronessimulator.gameengine.gamestatemanager;
 
 import org.inaetics.dronessimulator.gameengine.common.state.GameEntity;
 
+/**
+ * Interface for game state managers.
+ */
 public interface IGameStateManager {
+    /**
+     * Gets the game entity state for the entity with the given id.
+     * @param id The entity id.
+     * @return The game entity state.
+     */
     GameEntity getById(Integer id);
 
+    /**
+     * Adds a new entity state.
+     * @param gameEntity The entity state to add.
+     */
     void addEntityState(GameEntity gameEntity);
 
+    /**
+     * Removes the entity state for the given id.
+     * @param entityId The entity id to remove.
+     */
     void removeState(Integer entityId);
 }

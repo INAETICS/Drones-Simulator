00b5ba6df237ce88c22bce897f3f2272d481a1ad
Wybren Kortstra <wybren.kortstra@gmail.com>
2017-06-26 13:49:24 +0200

    Code cleanup

--- implementation/visualisation/src/main/java/org/inaetics/dronessimulator/visualisation/messagehandlers/StateMessageHandler.java
+++ implementation/visualisation/src/main/java/org/inaetics/dronessimulator/visualisation/messagehandlers/StateMessageHandler.java
@@ -34,11 +34,9 @@ public class StateMessageHandler implements MessageHandler {
      * @return drone Drone - The newly created drone
      */
     private Drone createPlayer(String id) {
-        // create drone
         BasicDrone drone = new BasicDrone(uiUpdates, canvas);
         drone.setPosition(new D3Vector(500, 400, 1000));
         drone.setDirection(new D3PoolCoordinate(0, 0, 0));
-
         return drone;
     }
 
@@ -49,12 +47,9 @@ public class StateMessageHandler implements MessageHandler {
      * @return bullet Bullet - The newly created bullet
      */
     private Bullet createBullet(String id) {
-        // create drone
         Bullet bullet = new Bullet(uiUpdates, canvas);
-
         bullet.setPosition(new D3Vector(0, 0, 0));
         bullet.setDirection(new D3PoolCoordinate(0, 0, 0));
-
         return bullet;
     }
 

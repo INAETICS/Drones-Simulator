ea7dc456d244a282904e8f9f4beb20c0a52c7994
Mart <martoudeweernink@gmail.com>
2017-06-28 15:01:50 +0200

    Unregister RabbitMQ from etcd when stopping Docker container. Dynamic wait for start of RabbitMQ server.

--- docker_images/rabbitmq/register_rabbitmq.sh
+++ docker_images/rabbitmq/register_rabbitmq.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-
 rabbitmq-server -detached
-echo "RabbitMQ started"
+echo "RabbitMQ starting ..."
 
+while ! nc -z localhost 5672; do   
+  sleep 0.1 # wait for 1/10 of the second before check again
+done
 
-sleep 10
-
+echo "RabbitMQ is gestart"
 
 USERNAME=yourUser
 PASSWORD=yourPass
 
-# add rabbitmq user
+	# add rabbitmq user
 rabbitmqctl add_user $USERNAME $PASSWORD
 rabbitmqctl set_permissions -p / $USERNAME ".*" ".*" ".*"
 rabbitmqctl set_user_tags $USERNAME administrator
 
-echo "User $USERNAME toegevoegd aan rabbitmq"
+echo "User $USERNAME toegevoegd aan RabbitMQ"
 
 # etcd paths
 INSTANCE_DIR="/instances/rabbitmq/broker/default"
@@ -23,7 +24,7 @@ INSTANCE_DIR="/instances/rabbitmq/broker/default"
 # RabbitMQ config
 RABBITMQ_PATH="amqp://rabbitmq:5672"
 
-# Actually set values
+# Actually set values in etcd
 curl http://etcd:4001/v2/keys$INSTANCE_DIR/uri -XPUT -d value="$RABBITMQ_PATH"
 curl http://etcd:4001/v2/keys$INSTANCE_DIR/username -XPUT -d value=$USERNAME
 curl http://etcd:4001/v2/keys$INSTANCE_DIR/password -XPUT -d value=$PASSWORD
@@ -31,11 +32,21 @@ curl http://etcd:4001/v2/keys$INSTANCE_DIR/password -XPUT -d value=$PASSWORD
 # Confirmation
 echo "RabbitMQ registered with URI $RABBITMQ_PATH"
 
-rabbitmqctl stop
 
-sleep 2
 
-rabbitmq-server start
+function finish {
+  # Your cleanup code here
+  echo "Remove RabbitMQ from etcd"
+  curl http://etcd:4001/v2/keys$INSTANCE_DIR/uri -XDELETE
+  curl http://etcd:4001/v2/keys$INSTANCE_DIR/username -XDELETE
+  curl http://etcd:4001/v2/keys$INSTANCE_DIR/password -XDELETE
+
+  echo "Stop RabbitMQ server"
+  rabbitmqctl stop
+  
+}
+trap finish SIGTERM
+while true; do :; done
 
 
 

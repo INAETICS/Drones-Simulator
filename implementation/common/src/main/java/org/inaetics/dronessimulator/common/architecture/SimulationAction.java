977df2b6a67f300dac0ad1fe087504e2252abc7a
Sebastiaan la Fleur <sebastiaan@slafleur.nl>
2017-06-29 17:54:43 +0200

    Added doc to action and state constants

--- implementation/common/src/main/java/org/inaetics/dronessimulator/common/architecture/SimulationAction.java
+++ implementation/common/src/main/java/org/inaetics/dronessimulator/common/architecture/SimulationAction.java
@@ -4,13 +4,40 @@ package org.inaetics.dronessimulator.common.architecture;
  * An action to be taken by the architecture
  */
 public enum SimulationAction {
+    /**
+     * Initialize the architecture
+     * Is taken only when the architecture transitions with (NOSTATE, INIT, INIT)
+     * This only happens when the initial state is first published to storage
+     */
     INIT ("init"),
+    /**
+     * Configure the architecture
+     */
     CONFIG ("config"),
+    /**
+     * Start the simulation
+     */
     START ("start"),
+    /**
+     * Stop the simulation
+     */
     STOP ("stop"),
+    /**
+     * Pause the simulation
+     */
     PAUSE ("pause"),
+    /**
+     * Resume the simulation after pause
+     */
     RESUME ("resume"),
+    /**
+     * Wincondition! Simulation is done
+     */
     GAMEOVER ("gameover"),
+    /**
+     * Destroy the architecture
+     * Is called only when the published current state is removed from storage
+     */
     DESTROY("destroy");
 
     /**
--- implementation/common/src/main/java/org/inaetics/dronessimulator/common/architecture/SimulationState.java
+++ implementation/common/src/main/java/org/inaetics/dronessimulator/common/architecture/SimulationState.java
@@ -4,11 +4,29 @@ package org.inaetics.dronessimulator.common.architecture;
  * A state the architecture can take
  */
 public enum SimulationState {
+    /**
+     * No state can be found currently
+     */
     NOSTATE ("nostate"),
+    /**
+     * Simulation is in the initial state / lobby
+     */
     INIT ("init"),
+    /**
+     * Simulation is configured
+     */
     CONFIG ("config"),
+    /**
+     * Simulation is running
+     */
     RUNNING ("running"),
+    /**
+     * Simulation is over
+     */
     DONE ("done"),
+    /**
+     * Simulation is paused
+     */
     PAUSED ("paused");
 
     /**

4df7d79b88fe4318b988d5ce59ade94752e57a1b
Sebastiaan la Fleur <sebastiaan@slafleur.nl>
2017-07-06 23:56:50 +0200

    Removed docker_images folder from repo. Added debug level as config for visualization

--- visualization/start_visualization.sh
+++ visualization/start_visualization.sh
@@ -4,4 +4,4 @@ source config_visualization
 export ETCD_HOST
 export ETCD_PORT
 
-java -jar visualisation-0.1.jar
+java -jar -Dorg.ops4j.pax.logging.DefaultServiceLog.level=$DEBUG_LEVEL visualisation-0.1.jar

960d1d0f8d8600232fbb9462ebd41f821c65d870
Martijn <m.j.willemsen@student.utwente.nl>
2017-10-05 10:58:52 +0200

    You do not need the special start script anymore

--- docker_start_images.sh
+++ docker_start_images.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-docker start dronessimulator_etcd_1
-docker start dronessimulator_rabbitmq_1
-sleep 2s
-docker start dronessimulator_game-engine_1
-docker start dronessimulator_architecture-manager_1
-
-docker-compose up -d --scale drone=4 drone
+docker-compose up -d

bb25fe17f1327bf73817c1437afe41a8f667d0e1
Martijn <m.j.willemsen@student.utwente.nl>
2017-12-15 11:19:47 +0100

    Some documentation

---
+++ implementation/drone/tactic/src/main/java/org/inaetics/dronessimulator/drone/tactic/example/utility/package-info.java
@@ -0,0 +1,7 @@
+
+/**
+ * This package contains an example tactic that uses a utility function. In the package messages there are the messages that are send between the drones. Note that
+ * drones are only able to receive TacticMessages. This contains a Map with String key-value pairs. The CalculateUtilityHelper does the actual utility calculations so
+ * you could copy the whole package to your own package and modify only that class.
+ */
+package org.inaetics.dronessimulator.drone.tactic.example.utility;

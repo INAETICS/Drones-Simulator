6f5fe51eaeb42c74fa45441c38784cac38147937
Tim108 <tsonderen108@gmail.com>
2018-01-19 13:49:51 +0100

    Basic tactic patrolling in formation

--- implementation/drone/tactic/src/main/java/org/inaetics/dronessimulator/drone/tactic/example/basic/BasicTactic.java
+++ implementation/drone/tactic/src/main/java/org/inaetics/dronessimulator/drone/tactic/example/basic/BasicTactic.java
@@ -5,10 +5,12 @@ import org.inaetics.dronessimulator.common.Settings;
 import org.inaetics.dronessimulator.common.TimeoutTimer;
 import org.inaetics.dronessimulator.common.Tuple;
 import org.inaetics.dronessimulator.common.vector.D3Vector;
+import org.inaetics.dronessimulator.drone.components.radar.Radar;
 import org.inaetics.dronessimulator.drone.tactic.Tactic;
 
 import java.time.LocalDateTime;
 import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Log4j
 public class BasicTactic extends Tactic {
@@ -33,6 +35,14 @@ public class BasicTactic extends Tactic {
     BasicTacticHeartbeat heartbeat;
     Thread commThread;
     Thread heartbeatThread;
+
+    int randomXmin = (int) Math.min(Radar.RADAR_RANGE, Settings.ARENA_WIDTH / 2);
+    int randomYmin = (int) Math.min(Radar.RADAR_RANGE, Settings.ARENA_DEPTH / 2);
+    int randomZmin = (int) Math.min(Radar.RADAR_RANGE, Settings.ARENA_HEIGHT / 2);
+    int randomXmax = (int) Math.max(Settings.ARENA_WIDTH - Radar.RADAR_RANGE, Settings.ARENA_WIDTH / 2 + 1);
+    int randomYmax = (int) Math.max(Settings.ARENA_DEPTH - Radar.RADAR_RANGE, Settings.ARENA_DEPTH / 2 + 1);
+    int randomZmax = (int) Math.max(Settings.ARENA_HEIGHT - Radar.RADAR_RANGE, Settings.ARENA_HEIGHT / 2 + 1);
+
 
     @Override
     protected void initializeTactics() {
@@ -65,9 +75,8 @@ public class BasicTactic extends Tactic {
         }
 
         if (isRadar) {
-            if (moveTarget == null) {
-//                moveTarget = new D3Vector(ThreadLocalRandom.current().nextInt(100, 300), ThreadLocalRandom.current().nextInt(100, 300), ThreadLocalRandom.current().nextInt(100, 300));
-                radar.getNearestTarget().ifPresent(x -> moveTarget = getDistanceTarget(x, 80));
+            if (moveTarget == null || getEnemy().isPresent()) {
+                moveTarget = getEnemy().isPresent() ? getDistanceTarget(getEnemy().get(), 80) : new D3Vector(ThreadLocalRandom.current().nextInt(randomXmin, randomXmax), ThreadLocalRandom.current().nextInt(randomYmin, randomYmax), ThreadLocalRandom.current().nextInt(randomZmin, randomZmax));
             }
 
             if (lastMoveTarget == null || !lastMoveTarget.equals(moveTarget) || myGunDronesChanged) {
@@ -84,7 +93,7 @@ public class BasicTactic extends Tactic {
         calculateMovements();
     }
 
-    private D3Vector getDistanceTarget(D3Vector target, int distance){
+    private D3Vector getDistanceTarget(D3Vector target, int distance) {
         D3Vector curDistance = target.sub(gps.getPosition());
         D3Vector direction = curDistance.normalize();
         D3Vector distanceVector = direction.scale(distance);
@@ -93,9 +102,14 @@ public class BasicTactic extends Tactic {
         return newTarget;
     }
 
-    protected void calculateMovements() {
+    private void calculateMovements() {
         if (moveTarget != null) {
-            engine.changeVelocity(moveTarget.sub(gps.getPosition()));
+            if (moveTarget.sub(gps.getPosition()).length() < 2) {
+                moveTarget = null;
+                engine.changeVelocity(new D3Vector());
+            } else {
+                engine.changeVelocity(moveTarget.sub(gps.getPosition()));
+            }
         }
     }
 
@@ -130,10 +144,10 @@ public class BasicTactic extends Tactic {
     }
 
     private void shoot() {
-        if (attackTarget != null) {
-            gun.fireBullet(attackTarget.sub(gps.getPosition()).toPoolCoordinate());
-            attackTarget = null;
-        }
+//        if (attackTarget != null) {
+//            gun.fireBullet(attackTarget.sub(gps.getPosition()).toPoolCoordinate());
+//            attackTarget = null;
+//        }
     }
 
     private void organizeMovement() {
@@ -166,9 +180,10 @@ public class BasicTactic extends Tactic {
         Map<String, Tuple<LocalDateTime, D3Vector>> teammembers = new HashMap<>();
         teammembers.putAll(radarDrones);
         teammembers.putAll(gunDrones);
-        return radar.getRadar().parallelStream()
-                .filter(ral -> //Get positions of the teammembers
-                        teammembers.entrySet().parallelStream().map(tm -> tm.getValue().getRight()).noneMatch(t -> t.distance_between(ral) < Settings.MAX_DRONE_VELOCITY)).min(Comparator.comparingDouble(e -> e.distance_between(gps.getPosition())));
+        return Optional.empty();
+//        return radar.getRadar().parallelStream()
+//                .filter(ral -> //Get positions of the teammembers
+//                        teammembers.entrySet().parallelStream().map(tm -> tm.getValue().getRight()).noneMatch(t -> t.distance_between(ral) < Settings.MAX_DRONE_VELOCITY)).min(Comparator.comparingDouble(e -> e.distance_between(gps.getPosition())));
     }
 
 }
